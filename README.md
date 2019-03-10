b58e17e716903d122c71d4be4782fddfecf1b7d7
meucaa <lucas.vuillier@gmail.com>
2019-03-10 19:39:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# zenlectricity-ui
+[![Build Status](https://travis-ci.org/meucaa/zenlectricity-ui.svg?branch=master)](https://travis-ci.org/meucaa/zenlectricity-ui)
+
+In the project directory, you can run:
+
+- `npm start`
+- `npm test`
+- `npm run build`
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1>Zenlectricity</h1>
+        </header>
+        <LoginForm/>
+      </div>
+    );
+  }
+}
+
+export default App;
