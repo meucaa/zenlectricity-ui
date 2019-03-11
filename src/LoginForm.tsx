9882054d56ce95e0b6f834bf8808b751293dc0db
meucaa <lucas.vuillier@gmail.com>
2019-03-11 22:06:18 +0100

    Basic form code in LoginForm

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,15 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Form.css';
 
-class LoginForm extends Component {
+interface LoginState {
+  username: string;
+  password: string;
+}
+
+
+class LoginForm extends React.Component<any, LoginState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {username: '', password: ''};
+
+    this.handleLoginChange = this.handleLoginChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleLoginChange(event: any) {
+    this.setState({username: event.target.value, password: this.state.password});
+  }
+
+  handlePasswordChange(event: any) {
+    this.setState({username: this.state.username, password: event.target.value});
+  }
+
+  handleSubmit(event: any) {
+    alert('You try to connect with: ' + JSON.stringify(this.state));
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="login-page">
         <div className="form">
           <form className="login-form">
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
-            <button>login</button>
+            <input type="text" onChange={this.handleLoginChange} placeholder="username"/>
+            <input type="password" onChange={this.handlePasswordChange} placeholder="password"/>
+            <button onClick={this.handleSubmit}>login</button>
             <p className="message">Not registered? <a href="#">Create an account</a></p>
           </form>
         </div>
