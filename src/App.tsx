e7f7ceba3d5e25ade053eb708c2ec292f5c8ace2
meucaa <lucas.vuillier@gmail.com>
2019-03-20 20:47:27 +0100

    Using hooks (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LoginForm from './LoginForm';
+import { LoginForm } from './LoginForm';
 
 class App extends Component {
   render() {
--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, MouseEvent } from 'react';
 import axios from 'axios';
 import './Form.css';
 
@@ -8,29 +8,22 @@ interface LoginState {
 }
 
 
-class LoginForm extends React.Component<any, LoginState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {username: '', password: ''};
+export const LoginForm = () => {
+  const [state, setState] = React.useState<LoginState>({username: '', password: ''});
 
-    this.handleLoginChange = this.handleLoginChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  function handleLoginChange(event: ChangeEvent<HTMLInputElement>) {
+    setState({username: event.target.value, password: state.password});
   }
 
-  handleLoginChange(event: any) {
-    this.setState({username: event.target.value, password: this.state.password});
+  function handlePasswordChange(event: ChangeEvent<HTMLInputElement>) {
+    setState({username: state.username, password: event.target.value});
   }
 
-  handlePasswordChange(event: any) {
-    this.setState({username: this.state.username, password: event.target.value});
-  }
-
-  handleSubmit(event: any) {
+  function handleSubmit(event: MouseEvent<HTMLButtonElement>) {
     axios.post('http://localhost:9000/api/login', {
-      login: this.state.username,
-      password: this.state.password
-    }).then(response => {
+      login: state.username,
+      password: state.password
+    }).then(_ => {
       alert("You got a token!");
     }, error => {
       alert("You got no token: " + error)
@@ -38,20 +31,16 @@ class LoginForm extends React.Component<any, LoginState> {
     event.preventDefault();
   }
 
-  render() {
-    return (
-      <div className="login-page">
-        <div className="form">
-          <form className="login-form">
-            <input type="text" onChange={this.handleLoginChange} placeholder="username"/>
-            <input type="password" onChange={this.handlePasswordChange} placeholder="password"/>
-            <button onClick={this.handleSubmit}>login</button>
-            <p className="message">Not registered? <a href="#">Create an account</a></p>
-          </form>
-        </div>
+  return (
+    <div className="login-page">
+      <div className="form">
+        <form className="login-form">
+          <input type="text" onChange={handleLoginChange} placeholder="username"/>
+          <input type="password" onChange={handlePasswordChange} placeholder="password"/>
+          <button onClick={handleSubmit}>login</button>
+          <p className="message">Not registered? <a href="#">Create an account</a></p>
+        </form>
       </div>
-    );
-  }
-}
-
-export default LoginForm;
+    </div>
+  );
+};
