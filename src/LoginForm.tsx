fdfc791dbe73ee4712a0fcc2ed75a1e43afda2c2
meucaa <lucas.vuillier@gmail.com>
2019-03-20 23:18:06 +0100

    Remove axios

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, MouseEvent } from 'react';
-import axios from 'axios';
 import { css } from './FormStyle';
 
 interface LoginState {
@@ -20,13 +19,20 @@ export const LoginForm = () => {
   }
 
   function handleSubmit(event: MouseEvent<HTMLButtonElement>) {
-    axios.post('http://localhost:9000/api/login', {
-      login: state.username,
-      password: state.password
-    }).then(_ => {
-      alert("You got a token!");
-    }, error => {
-      alert("You got no token: " + error)
+    fetch("http://localhost:9000/api/login", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        login: state.username,
+        password: state.password
+      })
+    }).then(response => {
+      if(response.status == 200) return response.json()
+      else return {status: response.status}
+    })
+    .then(jsonData => alert(`You got a response from the API: ${jsonData.token || jsonData.status}`))
+    .catch(error => {
+      alert(`No response from API: ${error}`)
     })
     event.preventDefault();
   }
